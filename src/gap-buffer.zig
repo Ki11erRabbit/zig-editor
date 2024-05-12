d604b5082e76a2b0b85c090227063134b929ac2f
ki11errabbit <unlikelytitan@gmail.com>
2024-05-12 11:42:45 -0600

    fixed gap buffer errors

--- src/gap-buffer.zig
+++ src/gap-buffer.zig
@@ -1,6 +1,8 @@
 const std = @import("std");
 const Allocator = std.mem.Allocator;
 
+const Error = error{OutOfBounds} || Allocator.Error;
+
 pub fn GapBuffer(comptime T: type) type {
     const ItemTag = enum {
         item,
@@ -10,18 +12,17 @@ pub fn GapBuffer(comptime T: type) type {
     return struct {
         const Self = @This();
         buffer: []Item,
+        length: usize,
         original_size: usize,
         gap_start: usize,
         gap_end: usize,
         gap_size: usize,
-        allocator: *Allocator,
-        pub fn init(allocator: *Allocator, size: usize) Allocator.Error!Self {
-            const buffer = try allocator.alloc(T, size);
-            if (buffer == null) {
-                return null;
-            }
+        allocator: *const Allocator,
+        pub fn init(allocator: *const Allocator, size: usize) Error!Self {
+            const buffer = try allocator.alloc(Item, size);
             return GapBuffer(T){
                 .buffer = buffer,
+                .length = size,
                 .original_size = size,
                 .gap_start = 0,
                 .gap_end = size,
@@ -30,21 +31,18 @@ pub fn GapBuffer(comptime T: type) type {
             };
         }
         pub fn deinit(this: Self) void {
-            this.allocator.dealloc(this.buffer);
+            this.allocator.free(this.buffer);
         }
-        pub fn grow_gap(this: *Self) Allocator.Error!void {
-            var new_size = this.buffer.length + this.original_size;
-            var new_buffer = try this.allocator.alloc(T, new_size);
-            if (new_buffer == null) {
-                return Allocator.Error.OutOfMemory;
-            }
-            var offset = 0;
+        pub fn grow_gap(this: *Self) Error!void {
+            var new_size = this.length + this.original_size;
+            var new_buffer = try this.allocator.alloc(Item, new_size);
+            var offset: usize = 0;
             var set_new_gap_start = false;
             var set_new_gap_end = false;
             for (new_buffer, 0..) |_, i| {
-                if (i >= this.buffer.length and offset == 0) {
-                    new_buffer[i] = Item{ .gap = void };
-                } else if (i >= this.buffer.length) {
+                if (i >= this.length and offset == 0) {
+                    new_buffer[i] = Item.gap;
+                } else if (i >= this.length) {
                     new_buffer[i] = this.buffer[i - offset];
                     if (!set_new_gap_end) {
                         this.gap_end = i;
@@ -65,14 +63,17 @@ pub fn GapBuffer(comptime T: type) type {
                     }
                 }
             }
+            this.allocator.free(this.buffer);
+            this.buffer = new_buffer;
+            this.length = new_size;
         }
-        pub fn insert(this: *Self, values: []const T, position: usize) Allocator.Error!void {
-            if (position < this.buffer.length or position >= this.buffer.length) {
-                return Allocator.Error.OutOfBounds;
-            } else if (position + values.length >= this.buffer.length) {
-                return Allocator.Error.OutOfBounds;
+        pub fn insert(this: *Self, values: []const T, length: usize, position: usize) Error!void {
+            if (position >= this.length) {
+                return Error.OutOfBounds;
+            } else if (position + length >= this.length) {
+                return Error.OutOfBounds;
             }
-            if (this.gap_size < values.length) {
+            if (this.gap_size < length) {
                 try this.grow_gap();
             }
             var index = position;
@@ -91,5 +92,28 @@ pub fn GapBuffer(comptime T: type) type {
                 try this.grow_gap();
             }
         }
+        pub fn move_gap_left(this: *Self, position: usize) void {
+            while (this.gap_start > position) {
+                this.gap_start -= 1;
+                this.gap_end -= 1;
+                this.buffer[this.gap_end] = this.buffer[this.gap_start];
+                this.buffer[this.gap_start] = Item.gap;
+            }
+        }
+        pub fn move_gap_right(this: *Self, position: usize) void {
+            while (this.gap_end < position) {
+                this.buffer[this.gap_start] = this.buffer[this.gap_end];
+                this.gap_start += 1;
+                this.gap_end += 1;
+            }
+        }
     };
+}
+
+test "test init" {
+    var buffer = try GapBuffer(i32).init(&std.testing.allocator, 10);
+    defer buffer.deinit();
+    try buffer.insert(&[_]i32{ 1, 2, 3 }, 3, 0);
+    try std.testing.expectEqual(@as(i32, 1), buffer.buffer[0].item);
+    try std.testing.expectEqual(@as(i32, 2), buffer.buffer[1].item);
 }
